b733be67858b79dd4dcefde3ab813c7f0ab6664d
Hsen <hsen.itw123@gmail.com>
2026-08-16 00:24:58 +0300

    First Check point

--- fetch_job.py
+++ fetch_job.py
@@ -1,10 +1,15 @@
-
 import requests
-a = requests.get("https://remotive.com/api/remote-jobs")
-if a.ok == True:
-    a = a.json()
-print(a)
-a = requests.get("https://remotive.com/api/remote-jobs")
-if a.ok == True:
-    a = a.json()
-print(a)
+jet = requests.get("https://remotive.com/api/remote-jobs")
+jet = jet.json()
+jobs = jet["jobs"]
+# jobs is a list containing 16 dicts.
+job_list = []
+for i in jobs:
+    id = i["id"]
+    url = i["url"]
+    title = i["title"]
+    company_name = i['company_name']
+    category = i["category"]
+    salary = i["salary"]
+    job_list.append([id, url, title, company_name, category, salary])
+print(job_list)
