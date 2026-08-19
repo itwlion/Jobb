bcce4cd1b1071f22dfa6ea2884a6d20cb1367b0f
Hsen <hsen.itw123@gmail.com>
2026-08-19 18:24:01 +0300

    now creats the table and fills it and saves it

--- jobradar/storage.py
+++ jobradar/storage.py
@@ -6,20 +6,11 @@ from api_client import every_job_title
 from api_client import every_job_category
 from api_client import every_job_date
 import sqlite3
-formatted_id = [(job_id,) for job_id in every_job_id]
-formatted_url = [(job_url,) for job_url in every_job__url]
-formatted_type = [(job_type,) for job_type in every_job_type]
-formatted_salary = [(job_salary,)for job_salary in every_job_salary]
-formatted_title = [(job_title,)for job_title in every_job_title]
-formatted_category = [(job_category,)for job_category in every_job_category]
-formatted_date = [(job_date,)for job_date in every_job_date]
 con = sqlite3.connect("JOBS.db")
 cur = con.cursor()
 cur.execute(
-    "CREATE TABLE IF NOT EXISTS jobs(id,url,title,category,job_type,date_posted)")
-cur.executemany("INSERT INTO jobs(id) VALUES(?)", formatted_id)
-cur.executemany("INSERT INTO jobs(url) VALUES(?)", formatted_url)
-cur.executemany("INSERT INTO jobs(title) VALUES(?)", formatted_title)
-cur.executemany("INSERT INTO jobs(category) VALUES(?)", formatted_category)
-cur.executemany("INSERT INTO jobs(job_type) VALUES(?)", formatted_type)
-cur.executemany("INSERT INTO jobs(date_posted) VALUES(?)", formatted_date)
+    "CREATE TABLE IF NOT EXISTS jobs(id PRIMARY KEY,url,title,category,job_type,date_posted,salary)")
+zipped_lists = zip(every_job_id, every_job__url, every_job_title,
+                   every_job_category, every_job_type, every_job_date, every_job_salary)
+cur.executemany('INSERT INTO jobs VALUES (?,?,?,?,?,?,?)', zipped_lists)
+con.commit()
