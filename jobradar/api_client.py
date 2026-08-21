70fb8bbe5ca95d9d8600df17677c95dbacb883ec
Hsen <hsen.itw123@gmail.com>
2026-08-21 16:09:20 +0300

    now uses get to give a value if not given

--- jobradar/api_client.py
+++ jobradar/api_client.py
@@ -51,10 +51,10 @@ every_job_date = []
 every_job_type = []
 every_job_salary = []
 for each_job in jobs:
-    every_job_id.append(each_job["id"])
-    every_job__url.append(each_job["url"])
-    every_job_category.append(each_job["category"])
-    every_job_date.append(each_job["publication_date"])
-    every_job_title.append(each_job["title"])
-    every_job_salary.append(each_job["salary"])
-    every_job_type.append(each_job["job_type"])
+    every_job_id.append(each_job.get("id"))
+    every_job__url.append(each_job.get("url"))
+    every_job_category.append(each_job.get("category", "Undefined"))
+    every_job_date.append(each_job.get("publication_date", "Undefined"))
+    every_job_title.append(each_job.get("title", "Undefined"))
+    every_job_salary.append(each_job.get("salary", "Undefined"))
+    every_job_type.append(each_job.get("job_type", "Undefined"))
