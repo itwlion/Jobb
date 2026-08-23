a9f6b0523a37dc5f614d6150498b5036b65bd375
Hsen <hsen.itw123@gmail.com>
2026-08-23 17:30:12 +0300

    the file for the commands

---
+++ jobradar/__main__.py
@@ -0,0 +1,18 @@
+from .api_client import fetch_jobs, ApiUnavailableError
+from .storage import save_jobs, get_jobs
+from .ai import analyze_job
+
+
+def main():
+    try:
+        jobs = fetch_jobs()
+        save_jobs(*jobs)
+    except ApiUnavailableError:
+        print("Could not connect to the job Api. Using Cache")
+    analyze_job()
+    jobs = get_jobs()
+    print(jobs)
+
+
+if __name__ == "__main__":
+    main()
