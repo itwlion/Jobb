c784a5f37dc18055a6edc4f23cc27ed16ed2ba5a
Hsen <hsen.itw123@gmail.com>
2026-08-24 13:29:30 +0300

    main logic

--- jobradar/__main__.py
+++ jobradar/__main__.py
@@ -1,12 +1,14 @@
+import argparse
 from .api_client import fetch_jobs, ApiUnavailableError
 from .storage import save_jobs, get_jobs
 from .ai import analyze_job
 
 
 def main():
+
     try:
         jobs = fetch_jobs()
-        save_jobs(*jobs)
+        save_jobs(jobs)
     except ApiUnavailableError:
         print("Could not connect to the job Api. Using Cache")
     analyze_job()
